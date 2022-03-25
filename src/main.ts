1aef713f5814888fefa592fe8fc6101d5383bd55
Luca Barone <baro.luc@gmail.com>
2022-03-25 17:13:33 +0100

    🚧 add router

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import Koa from 'koa'
+import {router} from './router'
 
 const port = 4000
 const server = new Koa()
 
-server.use((ctx) => {
-  ctx.status = 200
-  ctx.body = 'hello'
-})
+server.use(router.routes()).use(router.allowedMethods())
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
