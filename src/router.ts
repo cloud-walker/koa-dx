6aa784d31e823f65cc62304ca204e4cca8e4c9e8
Luca Barone <baro.luc@gmail.com>
2022-03-25 17:39:13 +0100

    🚧 add sqlite prisma db

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,15 @@
 import Router from '@koa/router'
+import {db} from './db'
 
 export const router = new Router()
 
-router.get('/', (ctx) => {
-  ctx.body = 'hello worlds'
+router.get('/articles', async (ctx) => {
+  const articles = await db.article.findMany()
+
+  ctx.body = {items: articles}
+  ctx.status = 200
 })
+
+// router.post('/articles', (ctx) => {
+
+// })
