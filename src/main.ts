6d3cb430a3eba26dbd41ca502280f600d11451ec
Luca Barone <baro.luc@gmail.com>
2022-04-01 13:52:04 +0200

    🚧 add body parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Koa from 'koa'
+import koaBody from 'koa-body'
 
 import {router} from './router'
 
 const port = 4000
 const server = new Koa()
 
+server.use(koaBody())
 server.use(router.routes()).use(router.allowedMethods())
 
 server.listen(port, () => {
