ad40a91a71e293a1af1d3dc99181ba1f36505d55
Luca Barone <baro.luc@gmail.com>
2022-04-01 18:07:32 +0200

    🚧 first working articles CRUD

--- src/article/article.router.ts
+++ src/article/article.router.ts
@@ -1,16 +1,51 @@
 import Router from '@koa/router'
 import {Article} from '@prisma/client'
+import {z} from 'zod'
 
 import {db} from '../db'
 
+const ParamsGetArticles = z.object({
+  limit: z.number().min(0).max(100).optional(),
+  search: z.string().optional(),
+})
+
+const ParamsGetArticle = z.object({
+  id: z.preprocess(Number, z.number().int()),
+})
+const ParamsUpdateArticle = ParamsGetArticle
+const ParamsDeleteArticle = ParamsGetArticle
+
+const PayloadUpsertArticle = z.object({
+  title: z.string().min(1),
+  content: z.string().optional(),
+  isPublished: z.boolean().default(false),
+})
+
 export const articleRouter = new Router({prefix: '/articles'})
 
+export type ParamsGetArticles = z.infer<typeof ParamsGetArticles>
 export type ResponseGetArticles = {data: Article[]}
 
+export type ParamsGetArticle = z.infer<typeof ParamsGetArticle>
 export type ResponseGetArticle = {data: Article}
 
+export type PayloadCreateArticle = z.infer<typeof PayloadUpsertArticle>
+export type ResponseCreateArticle = ResponseGetArticle
+
+export type ParamsUpdateArticle = z.infer<typeof ParamsUpdateArticle>
+export type PayloadUpdateArticle = PayloadCreateArticle
+export type ResponseUpdateArticle = ResponseGetArticle
+
+export type ParamsDeleteArticle = z.infer<typeof ParamsDeleteArticle>
+export type ResponseDeleteArticle = void
+
 articleRouter.get('/', async (ctx) => {
-  const articles = await db.article.findMany()
+  const searchParams = ParamsGetArticles.parse(ctx.params)
+
+  const articles = await db.article.findMany({
+    where: {title: {contains: searchParams.search ?? ''}},
+    take: searchParams.limit ?? 10,
+  })
   const body: ResponseGetArticles = {data: articles}
 
   ctx.body = body
@@ -18,10 +53,15 @@ articleRouter.get('/', async (ctx) => {
 })
 
 articleRouter.get('/:id', async (ctx) => {
-  const article = await db.article.findFirst()
+  const searchParams = ParamsGetArticle.parse(ctx.params)
+  const article = await db.article.findFirst({
+    where: {id: {equals: +searchParams.id}},
+  })
 
   if (article == null) {
-    throw Error('comment not found.')
+    ctx.body = {message: 'Article not found'}
+    ctx.status = 404
+    return
   }
 
   const body: ResponseGetArticle = {data: article}
@@ -31,6 +71,32 @@ articleRouter.get('/:id', async (ctx) => {
 })
 
 articleRouter.post('/', async (ctx) => {
-  ctx.body = {message: 'Route not implement, yet.'}
-  ctx.status = 400
+  const data = PayloadUpsertArticle.parse(ctx.request.body)
+  const article = await db.article.create({data})
+  const body: ResponseCreateArticle = {data: article}
+
+  ctx.body = body
+  ctx.status = 201
+})
+
+articleRouter.put('/:id', async (ctx) => {
+  const searchParams = ParamsGetArticle.parse(ctx.params)
+  const data = PayloadUpsertArticle.parse(ctx.request.body)
+  const article = await db.article.update({
+    where: {id: +searchParams.id},
+    data,
+  })
+  const body: ResponseUpdateArticle = {data: article}
+
+  ctx.body = body
+  ctx.status = 200
+})
+
+articleRouter.delete('/:id', async (ctx) => {
+  const searchParams = ParamsDeleteArticle.parse(ctx.params)
+  await db.article.delete({
+    where: {id: +searchParams.id},
+  })
+
+  ctx.status = 204
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa'
 import koaBody from 'koa-body'
+import {z} from 'zod'
 
 import {router} from './router'
 
@@ -7,6 +8,20 @@ const port = 4000
 const server = new Koa()
 
 server.use(koaBody())
+server.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      ctx.body = err.issues
+      ctx.status = 400
+      return
+    }
+
+    ctx.body = {message: 'Something went wrong.'}
+    ctx.status = 500
+  }
+})
 server.use(router.routes()).use(router.allowedMethods())
 
 server.listen(port, () => {
